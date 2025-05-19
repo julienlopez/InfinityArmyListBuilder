6d4cd9ec94b42908a226427fc9c8d1994fb19a39
Julien Lopez <julien.lopez51@gmail.com>
2025-05-19 20:46:00 +0200

    adding sectorials to the faction list

--- src/views/army_list.rs
+++ src/views/army_list.rs
@@ -2,13 +2,34 @@ use dioxus::prelude::*;
 
 use closure::closure;
 
-use crate::api::types::Metadata;
+use crate::api::types::{Faction, Metadata};
 
 const ARMY_LIST_CSS: Asset = asset!("/assets/styling/army_list.css");
+
+fn is_high_level_faction(faction: &Faction) -> bool {
+    faction.parent == faction.id
+}
+
+fn on_high_level_faction_clicked(faction: u64, mut unrolled_faction: Signal<Option<u64>>) {
+    let current = *unrolled_faction.clone().read();
+    match current {
+        Some(f) => {
+            if f == faction {
+                unrolled_faction.set(None);
+            } else {
+                unrolled_faction.set(Some(faction));
+            }
+        }
+        None => {
+            unrolled_faction.set(Some(faction));
+        }
+    }
+}
 
 #[component]
 pub fn ArmyList() -> Element {
     let metadata = use_server_future(echo_server)?;
+    let unrolled_faction: Signal<Option<u64>> = use_signal(|| None);
     let mut selected_faction: Signal<Option<u64>> = use_signal(|| None);
     rsx! {
         document::Link { rel: "stylesheet", href: ARMY_LIST_CSS }
@@ -17,11 +38,23 @@ pub fn ArmyList() -> Element {
                 match &*metadata.read_unchecked() {
                     Some(Ok(metadata)) => rsx! {
                         "Armies"
-                        for faction in &metadata.factions {
+                        for faction in metadata.factions.iter().filter(|f| is_high_level_faction(f)) {
                             div {
-                                class: "army_selection",
-                                onclick: closure!(clone faction, | _ | { * selected_faction.write() = Some(faction.id); }),
+                                class: "army_selection high_level",
+                                onclick: closure!(
+                                    clone faction, clone mut unrolled_faction, | _ | {
+                                    on_high_level_faction_clicked(faction.id, unrolled_faction.clone()); }
+                                ),
                                 "{faction.name}"
+                            }
+                            if *unrolled_faction.read() == Some(faction.id) {
+                                for faction in metadata.factions.iter().filter(|f| f.parent == faction.id) {
+                                    div {
+                                        class: "army_selection",
+                                        onclick: closure!(clone faction, | _ | { * selected_faction.write() = Some(faction.id); }),
+                                        "{faction.name}"
+                                    }
+                                }
                             }
                         }
                     },
